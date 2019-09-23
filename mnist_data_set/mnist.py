3b2e5cb8d0a3905213bf0c4315bccb16807cd5c2
unknown <mateusz.kotarski@gmail.com>
2019-09-23 23:03:31 +0200

    WIP

--- mnist_data_set/mnist.py
+++ mnist_data_set/mnist.py
@@ -12,6 +12,7 @@ import os
 import matplotlib.pyplot as plt 
 
 
+
 class MNIST():
 
     URL = 'https://storage.googleapis.com/cvdf-datasets/mnist/'
@@ -27,13 +28,13 @@ class MNIST():
     SHAPE = (IMAGE_SIZE, IMAGE_SIZE, 1)
 
     @staticmethod
-    def _download_file(file_name):
-        file = (os.path.splitext(file_name))[0]
-        if os.path.isfile(file):
+    def _download_file(file_name, destination):
+        temp_dest = destination + 'temp'
+        if os.path.isfile(destination):
             return
-        urllib.request.urlretrieve(filename = file_name, url = MNIST.URL + file_name)
-        with gzip.open(file_name, 'rb') as f_in:
-            with open(file, 'wb') as f_out: # remove .gz
+        urllib.request.urlretrieve(filename = temp_dest, url = MNIST.URL + file_name)
+        with gzip.open(temp_dest, 'rb') as f_in:
+            with open(destination, 'wb') as f_out: # remove .gz
                 shutil.copyfileobj(f_in, f_out)
 
 
@@ -41,23 +42,26 @@ class MNIST():
         mnist_dir = os.path.dirname(os.path.abspath(__file__))
         file = (os.path.join(mnist_dir, os.path.splitext(file_name)[0]))
         if not os.path.isfile(file):
-            MNIST.download(file_name)
+            MNIST._download_file(file_name, file)
         with open(file, 'rb') as file_handler:
             data = np.frombuffer(file_handler.read(), np.uint8, offset = offset)
         return data
 
-
-    @staticmethod
-    def download():
-        for file_name in MNIST.FILES_TO_GET:
-            MNIST._download_file(file_name)
-
     def plot_images(self):
-        fig, axes = plt.subplots(3, 3)
+        fig, axes = plt.subplots(2, 2)
         fig.subplots_adjust(hspace=0.3, wspace=0.3)
         for i, ax in enumerate(axes.flat):
             # Plot image.
-            ax.imshow(self.train_data[i+12].reshape((self.IMAGE_SIZE, self.IMAGE_SIZE)), cmap='binary')
+            pass
+            #ax.imshow(self.test_data[i+12].reshape((self.IMAGE_SIZE, self.IMAGE_SIZE)), cmap='binary')
+        #2
+        #ax.imshow(self.test_data[9779].reshape((self.IMAGE_SIZE, self.IMAGE_SIZE)), cmap='binary')
+        #5
+        ax.imshow(self.test_data[9980].reshape((self.IMAGE_SIZE, self.IMAGE_SIZE)), cmap='binary')
+        #2
+        #ax.imshow(self.test_data[9768].reshape((self.IMAGE_SIZE, self.IMAGE_SIZE)), cmap='binary')
+        #5
+        #ax.imshow(self.test_data[9719].reshape((self.IMAGE_SIZE, self.IMAGE_SIZE)), cmap='binary')
         '''
         # Show true and predicted classes.
         if cls_pred is None:
@@ -79,25 +83,39 @@ class MNIST():
         idx = np.random.randint(low=0, high=self.train_data.shape[0], size=batch_size)
         return self.train_data[idx], self.train_lables[idx] 
 
+    def get_train_batch_from_set(self, batch_size, set_num):
+        if set_num == 0:
+            idx = np.random.randint(low=0, high=self.nn_train_data_0.shape[0], size=batch_size)
+            return self.nn_train_data_0[idx], self.nn_train_lables_0[idx]
+        if set_num == 1:
+            idx = np.random.randint(low=0, high=self.nn_train_data_1.shape[0], size=batch_size)
+            return self.nn_train_data_1[idx], self.nn_train_lables_1[idx]
+
+    def get_test_set(self):
+        return self.test_data, self.test_data_labels
+
     #TODO: move to utils functions
     @staticmethod
     def one_hot_encoded(class_numbers, num_classes=None):
         if num_classes is None:
             num_classes = np.max(class_numbers) + 1
-
         return np.eye(num_classes, dtype=float)[class_numbers]
 
+    def get_num_train_samples(self):
+        return self.train_data.shape[0]
 
     def __init__(self):
         self.train_data = self._load_data(MNIST.TRAIN_IMAGES_FILES, MNIST.OFFSET_IMAGES)
         self.train_data  = self.train_data.reshape(-1, self.IMAGE_SIZE ** 2)/255
         self.train_lables = self._load_data(MNIST.TRAIN_LABELS_FILES, MNIST.OFFSET_LABELS)
 
+        self.nn_train_data_0 = self.train_data[0:int(self.train_data.shape[0] * 0.4)]
+        self.nn_train_data_1 = self.train_data[int(self.train_data.shape[0] * 0.4): int(self.train_data.shape[0] * 0.8)]
+        self.forest_train_data = self.train_data[int(self.train_data.shape[0] * 0.8):]
+        self.nn_train_lables_0 = self.train_lables[0:int(self.train_lables.shape[0] * 0.4)]
+        self.nn_train_lables_1 = self.train_lables[int(self.train_lables.shape[0] * 0.4):int(self.train_lables.shape[0] * 0.8)]
+        self.foreset_train_labels = self.train_lables[int(self.train_lables.shape[0] * 0.8):]
 
-def main():
-    test = MNIST()
- 
-
-if __name__ == '__main__':
-    main()
-
+        self.test_data = self._load_data(MNIST.TEST_IMAGE_FILES, MNIST.OFFSET_IMAGES)
+        self.test_data  = self.test_data.reshape(-1, self.IMAGE_SIZE ** 2)/255
+        self.test_data_labels = self._load_data(MNIST.TEST_LABELS_FILES, MNIST.OFFSET_LABELS)
--- trainer.py
+++ trainer.py
@@ -5,40 +5,54 @@ copyright (c) 2019 DATAUP Mateusz Kotarski 2019
 All rights reserved
 '''
 import os 
-
+import numpy as np
 from mnist_data_set.mnist import MNIST
 import tensorflow as tf
 
-data = tf.placeholder(tf.float32, [None, MNIST.IMAGE_SIZE **2])
-labels = tf.placeholder(tf.float32, [None, 10])
-weights = tf.Variable(tf.zeros([ MNIST.IMAGE_SIZE ** 2, 10]))
-biases = tf.Variable(tf.zeros([10]))
-logits = tf.matmul(data, weights) + biases
-y_pred = tf.nn.softmax(logits)
-y_pred_cls = tf.argmax(y_pred, axis=1)
-cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits,
-                                                           labels=labels)
-cost = tf.reduce_mean(cross_entropy)
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.05).minimize(cost)
+from models import ModelSimple
+import copy
+
+def train_net_self_teacher_target_label(target_label, data_set):
+    batch_size = 100
+    change = False
+    model = ModelSimple().from_scratch(num_outputs = 20, learning_rate = 0.3)
+    iter_num = 0
+    set_ = 0
+    while True:
+        batch_data, batch_labels = data_set.get_train_batch_from_set(batch_size, set_)
+        batch_labels_encoded = data_set.one_hot_encoded(batch_labels, 20)
+        model.train_on_batch(batch_data, batch_labels_encoded)
+        if iter_num >= data_set.get_num_train_samples() * 10 and not change:
+            change = True
+        if iter_num >= data_set.get_num_train_samples() * 10  and change:
+            iter_num = 0
+            test_data, test_labels = data_set.get_test_set()
+            test_labels_encoded = data_set.one_hot_encoded(test_labels, 20)
+            best_score = model.validate_self_teacher(test_data, test_labels_encoded , target_label)
+            model.save_model(target_label)
+            exit()
+            if set_ == 0:
+                test_labels = data_set.one_hot_encoded(data_set.nn_train_lables_0, 20)
+                new_labels = model.get_new_labels(data_set.nn_train_data_0, test_labels, target_label)
+                data_set.nn_train_lables_0 = new_labels
+                set_ = 1
+            if set == 1:
+                test_labels = data_set.one_hot_encoded(data_set.nn_train_lables_1, 20)
+                new_labels = model.get_new_labels(data_set.nn_train_data_1, test_labels, target_label)
+                data_set.nn_train_lables_1 = new_labels
+                set_ = 0
+        iter_num += batch_size
+
 
 def main():
     session = tf.Session()
     session.run(tf.global_variables_initializer())
-    batch_size = 100
     mnist_set = MNIST()
-    
-    for i in range(100):
-        batch_data, batch_labels = mnist_set.get_train_batch(batch_size)
-        batch_labels = mnist_set.one_hot_encoded(batch_labels, 10)
-        feed_dict_train = {data: batch_data,
-                           labels: batch_labels}
-        session.run(optimizer, feed_dict=feed_dict_train)
-        temp_logits = session.run(logits, feed_dict=feed_dict_train)
-        for idx, nic in enumerate(temp_logits):
-            print(nic)
-            print(batch_labels[idx])
-            print('*****')
+    for i in range(0, 10):
+        print("Running training for label: %d" %(i))
+        mnist_set = MNIST()
+        train_net_self_teacher_target_label(i, mnist_set)
 
-
+        
 if __name__ == '__main__':
     main()
